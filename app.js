dbf6e49c669c4a0a676fbbfce201159ec1794a92
oliverdangl <oliverdangl@gmail.com>
2025-06-13 16:37:05 +0200

    Refactored pug files for better readability

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+//const pg = require("pg");//Access to postgres db through Node.js
+//const bodyParser = require("body-parser");
 
 /* Reading global variables from config file */
 dotenv.config();
 const PORT = process.env.PORT;
+
+//const conString = process.env.DB_CON_STRING; //Includes parameters to connect
+
+/*
+//Forgot to set conString
+if(conString === undefined) {
+    console.log("Error: environment variable DB_CON_STRING not set.");
+    process.exit(1);
+}
+*/
+
+
+//Configure connection and connect to client
+//pg.defaults.ssl = true;
+//const dbClient = new pg.Client(conString);
+//dbClient.connect();
 
 app = express();
 
